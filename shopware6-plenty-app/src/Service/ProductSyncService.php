1ffa07307df6c926a7c265d345c9cdcfaa4f613a
ahmethamdi <ahmethamdik8@gmail.com>
2025-12-10 13:27:19 +0100

    Fix FileSaver parameter order (mediaId first)

--- shopware6-plenty-app/src/Service/ProductSyncService.php
+++ shopware6-plenty-app/src/Service/ProductSyncService.php
@@ -244,10 +244,10 @@ class ProductSyncService
 
             $this->fileSaver->persistFileToMedia(
                 $mediaFile,
+                $mediaId,
                 'product',
-                $fileName,
                 $context,
-                $mediaId
+                $fileName
             );
 
             return $mediaId;
